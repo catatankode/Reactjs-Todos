8b07fa2654d7cab90a9de8febd7fafe49103926c
rendiwijiatmoko <rendiwijiatmoko97@gmail.com>
2019-12-13 00:43:11 +0700

    todos, material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import TodoAdd from './components/TodoAdd';
+import { Typography } from '@material-ui/core';
+import TodoList from './components/TodoList';
+import useTodoState from './customeHooks/useTodoState';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  // const [todos, setTodos] = useState([]);
+  const {todos, addTodo, deleteTodo} = useTodoState([]);
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+      <TodoAdd
+        saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            // setTodos([...todos, trimmedText]);
+            addTodo(trimmedText);
+          }
+        }}
+      />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        // deleteTodo={(todoIndex) => {
+        //   const newTodos = todos.filter((_, index) => index !== todoIndex);
+        //   setTodos(newTodos);
+        // }}
+      />
     </div>
   );
 }
---
+++ src/components/TodoAdd.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const TodoAdd = ({saveTodo}) => {
+    const [text, setText] = useState("");
+
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                saveTodo(text);
+            }}
+        >
+            <TextField
+                variant="outlined"
+                placeholder="Add todo"
+                margin="normal"
+                onChange={(event) => {
+                    setText(event.target.value)
+                }}
+                value={text}
+            />
+        </form>
+    );
+}
+
+export default TodoAdd;
